6bf0aa9010dca728e59687f2763806d7d991002c
Noé <noehusser@gmail.com>
2025-04-02 12:02:53 +0200

    Tutorial test

--- dispatch_optimization.py
+++ dispatch_optimization.py
@@ -9,7 +9,7 @@ class BusElectricity():
         self.year = year
         self.name = f'{country} electriciy'
         self.network = pypsa.Network()
-        hours_in_year = pd.date_range(f'{self.year}01-01 00:00Z',
+        hours_in_year = pd.date_range(f'{self.year}-01-01 00:00Z',
                               f'{self.year}-12-31 23:00Z',
                               freq='h')
         self.network.set_snapshots(hours_in_year.values)
@@ -34,15 +34,15 @@ class BusElectricity():
         annualized_cost = utils.annuity(lifetime, 0.07)*capex*(1+opex_fixed/capex)
         carrier_name = technology_name
         self.network.add('Carrier', carrier_name, co2_emissions=CO2_emissions)
-        if data_prod:
+        if data_prod is not None:
             CF = data_prod[self.country][[hour.strftime('%Y-%m-%dT%H:%M:%SZ') for hour in self.network.snapshots]]
-            self.network.add('Generator', carrier=carrier_name, name=f'{self.country}_{technology_name}', 
+            self.network.add('Generator', carrier=carrier_name, name=carrier_name, 
                          bus = self.name, p_nom_extendable = True, capital_cost=annualized_cost, 
-                         marginal_cost=marginal_cost, efficiency=efficiency, p_max_pu=CF.values)
+                         marginal_cost=marginal_cost, p_max_pu=CF.values)
         else:
-            self.network.add('Generator', carrier=carrier_name, name=f'{self.country}_{technology_name}', 
+            self.network.add('Generator', carrier=carrier_name, name=carrier_name, 
                          bus = self.name, p_nom_extendable = True, capital_cost=annualized_cost, 
-                         marginal_cost=marginal_cost, efficiency=efficiency)
+                         marginal_cost=marginal_cost/efficiency)
 
     def add_co2_constraints(self):
         pass
@@ -57,29 +57,49 @@ class BusElectricity():
         self.objective_value = self.network.objective/1000000 # in 10^6 €
         self.electricity_price = self.network.objective/self.network.loads_t.p.sum()
 
+# Load Data
+df_onshorewind = pd.read_csv('data/onshore_wind_1979-2017.csv', sep=';', index_col = 0)
+df_onshorewind.index = pd.to_datetime(df_onshorewind.index)
+df_solar = pd.read_csv('data/pv_optimal.csv', sep=';', index_col=0)
+df_solar.index = pd.to_datetime(df_solar.index)
+
+france_net = BusElectricity('FRA', 2015)
+# Add onshore wind generator
+france_net.add_generator('onshorewind', 910000, 0.033*910000, 0, 30, 1, 0, df_onshorewind)
+
+# Add solar PV generator
+france_net.add_generator('solar', 425000, 0.03*425000, 0, 25, 1, 0, df_solar)
+
+# Add OCFT (Open Cycle Gas Turbine) generator
+france_net.add_generator('OCGT', 560000, 0.033*560000, 21.6, 25, 0.39, 10)
+
+# Optimize
+france_net.optimize()
+
+network = france_net.network
 
 
-# plt.plot(network.loads_t.p['load'][0:96], color='black', label='demand')
-# plt.plot(network.generators_t.p['onshorewind'][0:96], color='blue', label='onshore wind')
-# plt.plot(network.generators_t.p['solar'][0:96], color='orange', label='solar')
-# plt.plot(network.generators_t.p['OCGT'][0:96], color='brown', label='gas (OCGT)')
-# plt.legend(fancybox=True, loc='best')
-# plt.show()
+plt.plot(network.loads_t.p['load'][0:96], color='black', label='demand')
+plt.plot(network.generators_t.p['onshorewind'][0:96], color='blue', label='onshore wind')
+plt.plot(network.generators_t.p['solar'][0:96], color='orange', label='solar')
+plt.plot(network.generators_t.p['OCGT'][0:96], color='brown', label='gas (OCGT)')
+plt.legend(fancybox=True, loc='best')
+plt.show()
 
-# labels = ['onshore wind',
-#           'solar',
-#           'gas (OCGT)']
-# sizes = [network.generators_t.p['onshorewind'].sum(),
-#          network.generators_t.p['solar'].sum(),
-#          network.generators_t.p['OCGT'].sum()]
+labels = ['onshore wind',
+          'solar',
+          'gas (OCGT)']
+sizes = [network.generators_t.p['onshorewind'].sum(),
+         network.generators_t.p['solar'].sum(),
+         network.generators_t.p['OCGT'].sum()]
 
-# colors=['blue', 'orange', 'brown']
+colors=['blue', 'orange', 'brown']
 
-# plt.pie(sizes,
-#         colors=colors,
-#         labels=labels,
-#         wedgeprops={'linewidth':0})
-# plt.axis('equal')
+plt.pie(sizes,
+        colors=colors,
+        labels=labels,
+        wedgeprops={'linewidth':0})
+plt.axis('equal')
 
-# plt.title('France electricity mix', y=1.07)
-# plt.show()
+plt.title('France electricity mix', y=1.07)
+plt.show()
